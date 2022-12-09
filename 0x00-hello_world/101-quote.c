4e9142a382bab6e62ae1ac3a99c37a1dde937b79
Lumsta01 <lulamamanganyi01@gmail.com>
2022-12-08 19:33:34 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - line of code using write
  *
- * Return: 1
+ * Return: 0
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
