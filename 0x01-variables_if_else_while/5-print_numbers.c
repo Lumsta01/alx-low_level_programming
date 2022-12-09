40b6e0fa259b1958884c4017c978789cefc18509
Lumsta01 <lulamamanganyi01@gmail.com>
2022-12-09 13:11:53 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer from 0-10
+ *
+ * Return:0
+ */
+
+int main(void)
+
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
