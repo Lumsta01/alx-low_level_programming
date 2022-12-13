143515f6ad97ad5bf070a73e990ffab187cb3654
Lumsta01 <lulamamanganyi01@gmail.com>
2022-12-13 14:37:49 -0800

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print time from 00:00 to 23:59
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 00 : 00; i <= 23 : 59; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar("\n");
+
+}
