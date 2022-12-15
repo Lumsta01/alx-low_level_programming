a07719e0def13d5e348b3ca31e5ec4498835d103
Lumsta01 <lulamamanganyi01@gmail.com>
2022-12-15 08:53:47 -0800

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		_putchar(i);
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
 	}
-	_putchar("\n")
-
+	_putchar('\n');
 }
