b2798a561397f6a3e31c413c37104b1b6d97df19
Lumsta01 <lulamamanganyi01@gmail.com>
2022-12-20 13:46:04 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointer to an int updating the value of pounts to 98
+ *@n: parameter
+ *Return: 0
+ */
+
+int void reset_to_98(int *n);
+{
+	*n = 98;
+
+	return (0);
+}
+
+
