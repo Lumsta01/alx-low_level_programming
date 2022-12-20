e6dd5995861d008e54aea47f8d239c06e5811ba4
Lumsta01 <lulamamanganyi01@gmail.com>
2022-12-20 15:50:05 -0800

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - generating random valid passwords
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+
+	printf("%c", 2772 - sum);
+
+	return (0);
+}
